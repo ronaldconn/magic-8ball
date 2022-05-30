8e5a868c6a6e3b9a73fa99c78737113405d113d4
Poornima P K <poornima.pk@gmail.com>
2022-05-29 17:13:20 -0700

    First version of server.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-document.querySelector('#clickMe').addEventListener('click', makeReq)
+document.querySelector('.clickMe').addEventListener('click', makeReq)
 
 async function makeReq(){
 
-  const userName = document.querySelector("#userName").value;
-  const res = await fetch(`/api?student=${userName}`)
+  const res = await fetch(`/api`)
   const data = await res.json()
 
   console.log(data);
-  document.querySelector("#personName").textContent = data.name
-  document.querySelector("#personStatus").textContent = data.status
-  document.querySelector("#personOccupation").textContent = data.currentOccupation
+  document.querySelector(".textbox").textContent = data.answer;
 }
